0570c5e17d11c6312a9f39baf77a7380cc9bd8d9
isaque200025 <isaquealves.games@gmail.com>
2025-04-06 21:10:55 -0300

    Add files via upload

---
+++ SRC/index.js
@@ -0,0 +1,23 @@
+const botoes = document.querySelectorAll(".botao");
+const personagens = document.querySelectorAll(".personagem");
+
+
+botoes.forEach((botao, indice) => {
+    botao.addEventListener("click", () => {
+
+        const botaoSelecionado = document.querySelector(".botao.selecionado");
+        console.log(botaoSelecionado);
+        botaoSelecionado.classList.remove("selecionado");
+
+
+        botao.classList.add("selecionado");
+
+        const personagemSelecionado = document.querySelector(".personagem.selecionado");
+        personagemSelecionado.classList.remove("selecionado");
+
+        personagens[indice].classList.add("selecionado");
+
+    })
+});
+
+
